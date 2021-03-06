db366db1e99572c119e0c194d5e4f2fb34a7d1a3
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-03-06 04:49:58 -0500

    commit tree b44991de5c76830eab9bf83710dd99b8342c24bc

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
 
 class App extends Component {
-
   handleOnClickItems() {
     this.props.dispatch({
-      type: 'GET_COUNT_OF_ITEMS',
+      type: "GET_COUNT_OF_ITEMS",
     });
   }
 
   handleOnClickUsers() {
     this.props.dispatch({
-      type: 'GET_COUNT_OF_USERS',
+      type: "GET_COUNT_OF_USERS",
     });
   }
 
@@ -21,13 +19,13 @@ class App extends Component {
     // debugger;
     return (
       <div className="App">
-          <button onClick={() => this.handleOnClickItems()}>
-            Click to change items count
-            </button>
-          <button onClick={() => this.handleOnClickUsers()}>
-            Click to change user count
-          </button>
-          <p>{this.props.items.length}</p>
+        <button onClick={() => this.handleOnClickItems()}>
+          Click to change items count
+        </button>
+        <button onClick={() => this.handleOnClickUsers()}>
+          Click to change user count
+        </button>
+        <p>{this.props.items.length}</p>
       </div>
     );
   }
@@ -38,4 +36,15 @@ const mapStateToProps = (state) => {
   return { items: state.items }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(state => ({items: state.items}))(App)
+
+
+
+
+
+// const vanilla = (milkshake) => {
+//   debugger;
+//   return { items: milkshake.items };
+// };
+
+// export default connect(vanilla)(App);
